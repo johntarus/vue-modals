7dca4925fb63c897be28a4da9fd678bb12c4687d
tarus <tarusjohn96@gmail.com>
2022-11-08 11:51:22 +0300

    first commit

---
+++ src/App.vue
@@ -0,0 +1,29 @@
+<template>
+  <div v-if="showModal">
+    <Modal theme='sale' @close='toggleModal'>
+      <h1>Checking up on slot</h1>
+      <p>creating modals in vue is amazing</p>
+    </Modal>
+  </div>
+  <button @click="toggleModal">show modal</button>
+</template>
+
+<script>
+import Modal from './components/Modal.vue';
+
+export default {
+  components: { Modal },
+  data() {
+    return {
+      title: `Hello World Vue, I'm coming!`,
+      header: `many give aways on the way`,
+      showModal: false,
+     }
+  },
+  methods: {
+    toggleModal() {
+        this.showModal = !this.showModal
+      }
+    }
+  }
+</script>
---
+++ src/components/Modal.vue
@@ -0,0 +1,45 @@
+<template>
+    <div class="backdrop" @click.self="closeModal">
+        <div class="modal" :class="{sale: theme === 'sale'}">
+           <slot></slot>
+        </div>
+    </div>
+    
+</template>
+
+<script>
+export default {
+    props: ['theme'],
+    methods: {
+        closeModal() {
+            this.$emit('close')
+        }
+    }
+    }
+</script>
+<style scoped>
+.modal{
+    padding: 20px;
+    width: 400px;
+    text-align: center;
+    font-size: 20px;
+    background: white;
+    border-radius: 10px;
+    margin: 100px auto;
+}
+    .backdrop{
+        top: fixed;
+        width: 100%;
+        height: 100%;
+        background: rgba(0,0,0,0.5);
+        position: fixed;
+    }
+    .modal.sale {
+        background: crimson;
+        color: white;
+    }
+    .modal.sale h1 {
+        color: white;
+    }
+</style>
+
